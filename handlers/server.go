9ab16eef33719e6612d332a4a49ab4f9b0371238
stpotter16 <stpotter1642@gmail.com>
2024-11-25 09:44:21 -0500

    Add server struct

---
+++ handlers/server.go
@@ -0,0 +1,19 @@
+package handlers
+
+import "net/http"
+
+type Server struct {
+    router *http.ServeMux
+}
+
+func (s Server) Handler() http.Handler {
+    return s.router
+}
+
+func New() Server {
+    s := Server {
+        router: http.NewServeMux(),
+    }
+
+    return s
+}
